ccf8d4ae1c330f67d1843e38796ae9a55481f2c9
Jason Lin <jasonlin43212@gmail.com>
2025-04-04 00:10:07 -0400

    Fix next button again again

--- src/components/shared/MyButton.tsx
+++ src/components/shared/MyButton.tsx
@@ -12,6 +12,6 @@ export const MyButton: React.FC<ByButton> = (props: ByButton) => {
     onClick={() => props.onClick()}
     className={`my-button-wrapper ${props.back ? 'back-button-wrapper' : 'forward-button-wrapper'}`}
   >
-    <div className={"my-button-side-text"}>{props.back ? "← " : ""}{props.text}{!props.back ? "→" : ""}</div>
+    <div className={"my-button-side-text"}>{props.back ? "← " : ""}{props.text}{!props.back ? " →" : ""}</div>
   </div>
 } 
